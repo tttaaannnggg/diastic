dc9053a6121eae148aa45eeb2a2365eb34c5b21a
Tang <eytang8@gmail.com>
2020-04-04 22:56:01 -0500

    Updates

---
+++ precache-manifest.ec7e43ffad21c2f8c7e906162b3f35b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b6be5114fd0d53f3aa9a4bac9a2d52",
+    "url": "/diastic/index.html"
+  },
+  {
+    "revision": "6b3fc419ca3d533a4526",
+    "url": "/diastic/static/css/main.94b35dcf.chunk.css"
+  },
+  {
+    "revision": "43e47d269c618921b6c7",
+    "url": "/diastic/static/js/2.4118de47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diastic/static/js/2.4118de47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3fc419ca3d533a4526",
+    "url": "/diastic/static/js/main.9519b408.chunk.js"
+  },
+  {
+    "revision": "36c72a8bd876c06cd89b",
+    "url": "/diastic/static/js/runtime-main.381f77e0.js"
+  }
+]);
